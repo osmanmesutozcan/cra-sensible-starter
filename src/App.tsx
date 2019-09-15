2c07cf1b29e7abd46322e6c7414a2a7a3f9f6275
Osman Mesut Ozcan <osmanmesutozcan@gmail.com>
2019-09-15 11:04:35 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter, Route, RouteProps, Redirect} from 'react-router-dom'
+
 import './App.css';
+import {Home} from "./pages/Home";
+import {Auth} from "./pages/Auth";
+
+const PrivateRoute: React.FC<RouteProps> = ({component, ...rest}) => {
+  const Component = component as any;
+  const render = (props: any) =>
+      (false)
+          ? (<Component {...props} />)
+          : (<Redirect to={"/auth"} />);
+
+  return <Route {...rest} render={render} />
+};
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <PrivateRoute path="/" exact={true} component={Home} />
+        <Route path="/auth" exact={true} component={Auth} />
+      </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Auth.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export class Auth extends React.Component {
+  public render() {
+    return (
+        <h1>Auth</h1>
+    );
+  }
+}
